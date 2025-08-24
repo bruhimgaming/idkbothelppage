e2f762f2617437175ab37ab3d5ef56335b8d357a
bruhimgaming <hi8787872025@outlook.com>
2025-08-24 13:42:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Replace with your actual commands
   const commands = [
-    ";help - this motherfucker"
+    ";help - this motherfucker",
     ";hello - this was a test thing",
     ";credits - shows the credits",
-    ";time - idk it shows random numbers it was a mistake so i left it in"
-    ";fish - says gulb gulb idk"
+    ";time - idk it shows random numbers it was a mistake so i left it in",
+    ";fish - says gulb gulb idk",
     ";pasta - sends you a pasta image"
   ];
 
