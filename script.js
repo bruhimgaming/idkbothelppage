810e0f96fd1ca07ea5e3d49b95a766c9adcb7d76
bruhimgaming <hi8787872025@outlook.com>
2025-08-27 14:41:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,9 @@ document.addEventListener("DOMContentLoaded", () => {
     ";credits - shows the credits",
     ";time - idk it shows random numbers it was a mistake so i left it in",
     ";fish - says gulb gulb idk",
-    ";pasta - sends you a pasta image"
+    ";calc - calculator (cant subtract)"
+    ";roll - roll a dice"
+  ";room - only bruh and bensav bitch"
   ];
 
   commands.forEach(cmd => {
